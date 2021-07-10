8c22a4f6bc0b1b781a7b94d2a5b54c98eab2ac3a
Jana Abdelkader <janaosama1300@gmail.com>
2021-07-10 11:15:44 +0300

    modularized!

--- server.js
+++ server.js
@@ -13,71 +13,15 @@ const axios = require('axios');
 require('dotenv').config();
 
 
-class Forcast {
-    constructor(date,description,maxTemp,minTemp,cityName){
-        this.cityName = cityName;
-        this.date = date;
-        this.description = `Low of ${minTemp}, high of ${maxTemp} with ${description}`;
-    }  
-}
+
+const weatherServer = require('./modules/weather');
+const getMovies = require('./modules/movies');
 
 
-
-server.get('/weather',(request,response)=>{
-
-    let weatherDataFromAPI;
-    let requestedDataLink = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lon=${request.query.lon}&lat=${request.query.lat}`;
+server.get('/weather',weatherServer);
 
 
-    axios.get(requestedDataLink).then(weatherDataFromAPI => {
-
-        let arrayOfOneCityData =[];
-        console.log('weatherDataFromAPI.data.data');
-        weatherDataFromAPI.data.data.map((dayWeather)=>{
-                let WeatherDataForADay = new Forcast(dayWeather.datetime,dayWeather.weather.description,dayWeather.max_temp,dayWeather.low_temp,request.query.city);
-                arrayOfOneCityData.push(WeatherDataForADay);
-            })
-
-       console.log(arrayOfOneCityData);
-        response.status(200).send(arrayOfOneCityData);
-    });
-
-})                                                                              
-
-
-class Movies {
-    constructor(movieName,releaseDate,imageURL,overview,totalVotes,avgVotes,popularity){
-        this.movieName = movieName;
-        this.releaseDate = releaseDate;
-        this.imageURL = `https://image.tmdb.org/t/p/w500${imageURL}`;
-        this.overview = overview;
-        this.totalVotes = totalVotes;
-        this.avgVotes = avgVotes;
-        this.popularity = popularity;
-    }
-}
-
-server.get('/movies', (req,res)=>{
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${encodeURI(req.query.query)}`
-
-    let moviesDataArray = [];
-    axios.get(url).then(moviesData =>{
-        console.log(moviesData.data.results);
-        moviesData.data.results.map((movie)=>{
-            let dataFromMovie = new Movies(movie.title,movie.release_date,movie.poster_path,movie.overview,movie.vote_count,movie.vote_average,movie.vote_average,movie.popularity);
-            moviesDataArray.push(dataFromMovie);
-        })
-        res.send(moviesDataArray);
-    })
-
-    
-})
-
-
-
-server.get('/test',(req,res)=>{
-    res.send('hi')
-})
+server.get('/movies',getMovies);
 
 
 server.get('*',(req,res,error)=>{
@@ -86,5 +30,5 @@ server.get('*',(req,res,error)=>{
 
 
 server.listen(process.env.PORT,()=>{
-    console.log("working")
+    console.log("server listener is running")
 })
